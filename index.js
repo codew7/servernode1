1e07e844d99e0ff2a52492c96cd1c9b5e6322bf5
Alejandro <aleromer8@gmail.com>
2025-12-14 20:12:52 -0300

    Servidor Node inicial

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Servidor Node.js funcionando");
+  res.send("Servidor Node funcionando");
 });
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Servidor activo en puerto ${PORT}`);
+app.listen(PORT, () => {
+  console.log("Servidor activo en puerto " + PORT);
 });
