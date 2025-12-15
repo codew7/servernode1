8673a14ee9dcfc0d3186bae0ad908b75bfbf552a
Alejandro <aleromer8@gmail.com>
2025-12-14 21:03:37 -0300

    Usar puerto dinamico Railway

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+// OBLIGATORIO: usar SOLO el puerto de Railway
+const PORT = process.env.PORT;
 
 app.get("/", (req, res) => {
-  res.send("Servidor Node funcionando");
+  res.status(200).send("Servidor Node funcionando");
 });
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("Servidor activo en puerto " + PORT);
+  console.log("Servidor activo en puerto", PORT);
 });
